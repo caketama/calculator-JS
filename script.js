7123c028b8ba13156e035fb619a897da048609e8
cake <c4ke@m.me>
2019-11-27 13:07:20 -0500

    calculator

---
+++ script.js
@@ -0,0 +1,10 @@
+function multiply() {
+  let firstNum = document.getElementById("m1").value;
+  let secondNum = document.getElementById("m2").value;
+  document.getElementById("result").innerHTML = firstNum * secondNum;
+}
+function divide() {
+  let firstNum = document.getElementById("d1").value;
+  let secondNum = document.getElementById("d2").value;
+  document.getElementById("result").innerHTML = firstNum * secondNum;
+}
